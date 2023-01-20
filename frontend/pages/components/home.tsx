89dbe626efb925696c2f65bb63e7920fb07da4cc
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-01-20 02:32:45 -0500

    added framer motion into project

---
+++ frontend/pages/components/home.tsx
@@ -0,0 +1,16 @@
+
+export const HomePage = () => {
+
+    return(
+        <div className = "flex flex-col justify-center items-center w-full h-screen">
+            <h1 className = "p-20 text-6xl text-bold font-bold">
+                Canadian Uni Res Rater
+            </h1>
+
+            <button className = "py-3 px-5 border-4 bg-blue-300 border-blue-400 rounded-2xl">
+                Get started
+            </button>
+        </div>
+    )
+
+}
