4c8beef30718803aa74f53d07a9dfad13417efd2
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-01-20 20:20:19 -0500

    added fade in animation to the home page

--- frontend/pages/components/home.tsx
+++ frontend/pages/components/home.tsx
@@ -2,15 +2,19 @@
 export const HomePage = () => {
 
     return(
-        <div className = "flex flex-col justify-center items-center w-full h-screen">
-            <h1 className = "p-20 text-6xl text-bold font-bold">
-                Canadian Uni Res Rater
-            </h1>
+        <div id = "homediv" className = "animate-fade">
+            <div className = "flex flex-col justify-center items-center w-full h-screen">
+                <h1 className = "px-20 py-5 text-6xl text-bold font-bold ">
+                    Canadian Uni Res Rater
+                </h1>
 
-            <button className = "py-3 px-5 border-4 bg-blue-300 border-blue-400 rounded-2xl">
-                Get started
-            </button>
+                <button className = "py-3 px-5 border-4 bg-blue-300 border-blue-400 rounded-2xl animate-buttonFade">
+                    Get started
+                </button>
+            </div>
         </div>
+
+
     )
 
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: 'fadeIn 3s ease-in-out',
+        buttonFade: 'fadeButton 3s ease-in-out'
+      },
+      keyframes: theme => ({
+        fadeIn: {
+          '0%': { color: theme('colors.transparent'), backgroundColor: theme('color.transparent')},
+
+          '100%' : {color: theme('black')}
+        },
+
+        fadeButton: {
+          '0%': { color: theme('colors.transparent'), backgroundColor: theme('colors.transparent'), borderColor: theme('colors.transparent')},
+
+          '100%' : {color: theme('black'), backgroundColor: theme('colors.blue.300'), borderColor: theme('colors.blue.400')}
+
+        }
+
+
+      }),
+    },
   },
   plugins: [],
 }
